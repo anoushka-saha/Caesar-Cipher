4868e6995496e3f9e4b88780ea7ec6dc92ecaf89
anoushka1111 <saha.anoushka@gmail.com>
2024-05-08 09:06:31 -0400

    while loop for repeat usage

--- CaesarCipherFinal.py
+++ CaesarCipherFinal.py
@@ -19,17 +19,26 @@ def caesar(choice, text, num):
 
     return caesar_text
 
-#Ask user if they want message encrypted or decrypted
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+#Loop so user can continue using program
+while True: 
+    #Ask user if they want message encrypted or decrypted
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 
-#Ask user for message
-message = input("Type your message:\n").lower()
+    #Ask user for message
+    message = input("Type your message:\n").lower()
 
-#Ask user for amount of shift
-shift = int(input("Type the shift number:\n"))
+    #Ask user for amount of shift
+    shift = int(input("Type the shift number:\n"))
 
-#Print new message
-print("The new message is: " + caesar(direction, message, shift))
+    #Print new message
+    print("The new message is: " + caesar(direction, message, shift))
+
+    #Check if user wants to go again
+    go_again = input(print("Do you want to go again? (yes/no):\n"))
+
+    #Breaking loop if user wants to exit program
+    if go_again.lower() != "yes":
+         break
 
 
              
